6948c343147b9a24ec8c90d3c5621398fb04b7dd
Ton <tondev123@gmail.com>
2026-05-18 17:13:52 +0700

    cmc v7

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CMS PASSWORD RESET V6 20260518',
+      title: 'CMS PASSWORD RESET V7 20260518',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
